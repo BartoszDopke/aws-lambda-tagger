b1a8300ba2433694aecc9e249374c32c9d263032
Bartosz Dopke <bartek.dopke@wp.pl>
2025-08-31 06:49:30 +0200

    change IAM role name

--- iam.tf
+++ iam.tf
@@ -29,7 +29,7 @@ resource "aws_iam_policy" "tagging_policy" {
 }
 
 resource "aws_iam_role" "lambda_tagger" {
-  name               = "lambda_execution_role"
+  name               = "lambda_tagger_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
